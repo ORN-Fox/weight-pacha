67d32fdfd2d348d08746890dd3976c747d654c72
Florian Armand <49326517+ORN-Fox@users.noreply.github.com>
2024-08-11 21:47:03 -0400

    feat: review WeightMonitoringComponent

--- src/app/pages/weight-monitoring/weight-monitoring.component.ts
+++ src/app/pages/weight-monitoring/weight-monitoring.component.ts
@@ -14,7 +14,7 @@ import { Measure } from 'src/app/core/models/measure/measure.model';
 export class WeightMonitoringComponent {
 
   chart: any;
-  data;
+  data: any;
 
   measures: Measure[];
 
@@ -22,53 +22,12 @@ export class WeightMonitoringComponent {
   weight: number;
 
   constructor() {
+    this.measures = [];
+
     this.date = moment();
     this.weight = 0;
 
-    this.measures = [
-    ];
-
-    const down = (ctx: { p0: { parsed: { y: number; }; }; p1: { parsed: { y: number; }; }; }, value: string): string | undefined => {
-      if (ctx.p0.parsed.y > ctx.p1.parsed.y) {
-        return value;
-      }
-      return undefined;
-    }
-
-    const skipped = (ctx: { p0: { skip: any; }; p1: { skip: any; }; }, value: string | number[]): string | number[] | undefined => {
-      if (ctx.p0.skip || ctx.p1.skip) {
-        return value;
-      }
-      return undefined;
-    }
-
-    const dataPoints: any[] = [];
-    this.measures.forEach(measure => {
-      let dataPoint = {
-        x: measure.date,
-        y: measure.weigth
-      }
-      dataPoints.push(dataPoint);
-    });
-
-    this.data = {
-      datasets: [
-        {
-          label: 'Poids (Kg)',
-          data: dataPoints,
-          cubicInterpolationMode: 'monotone',
-          pointStyle: 'circle',
-          pointRadius: 5,
-          pointHoverRadius: 10,
-          borderColor: 'rgb(75, 192, 192)',
-          segment: {
-            borderColor: (ctx: any) => down(ctx, 'rgb(192,75,75)') || skipped(ctx, 'rgb(0,0,0,.2)'),
-            borderDash: (ctx: any) => skipped(ctx, [6, 6]),
-          },
-          spanGaps: true
-        }
-      ]
-    };
+    this.loadMeasures();
   }
 
   ngAfterViewInit() {
@@ -131,6 +90,78 @@ export class WeightMonitoringComponent {
     this.chart.data.datasets[0].data = this.chart.data.datasets[0].data.filter((dataPoint: { x: moment.MomentInput, y: number }) => !moment(dataPoint.x).isSame(measureToDelete.date, 'day'));
     this.chart.update();
   }
+
+  private loadMeasures() {
+    // TODO load from json file for storage
+    // TODO save in json file for storage
+    this.measures = [
+      new Measure(moment('2024-01-01'), 1),
+      new Measure(moment('2024-02-02'), 1.2),
+      new Measure(moment('2024-03-03'), 1.2),
+      new Measure(moment('2024-04-04'), 1.1),
+      new Measure(moment('2024-05-05'), null),
+      new Measure(moment('2024-06-06'), 1.1),
+      new Measure(moment('2024-07-07'), 1.3),
+      new Measure(moment('2024-08-08'), 0.87),
+      // new Measure(moment('2021-06-27'), .220),
+      // new Measure(moment('2021-12-27'), .832),
+      // new Measure(moment('2022-06-27'), 1.678),
+      // new Measure(moment('2022-12-27'), 2.234),
+      // new Measure(moment('2023-06-27'), 3.021),
+      // new Measure(moment('2023-12-27'), 3.894),
+      // new Measure(moment('2024-06-27'), 4.153)
+    ];
+
+    this.initChartData();
+  }
+
+  private initChartData() {
+    const down = (ctx: { p0: { parsed: { y: number; }; }; p1: { parsed: { y: number; }; }; }, value: string): string | undefined => {
+      if (ctx.p0.parsed.y > ctx.p1.parsed.y) {
+        return value;
+      }
+      return undefined;
+    }
+
+    const skipped = (ctx: { p0: { skip: any; }; p1: { skip: any; }; }, value: string | number[]): string | number[] | undefined => {
+      if (ctx.p0.skip || ctx.p1.skip) {
+        return value;
+      }
+      return undefined;
+    }
+
+    this.data = {
+      datasets: [
+        {
+          label: 'Poids (Kg)',
+          data: this.computeDataPoints(),
+          cubicInterpolationMode: 'monotone',
+          pointStyle: 'circle',
+          pointRadius: 5,
+          pointHoverRadius: 10,
+          borderColor: 'rgb(75, 192, 192)',
+          segment: {
+            borderColor: (ctx: any) => down(ctx, 'rgb(192,75,75)') || skipped(ctx, 'rgb(0,0,0,.2)'),
+            borderDash: (ctx: any) => skipped(ctx, [6, 6]),
+          },
+          spanGaps: true
+        }
+      ]
+    };
+  }
+
+  private computeDataPoints(): any[] {
+    let dataPoints: any[] = [];
+    this.measures.forEach(measure => {
+      let dataPoint = {
+        x: measure.date,
+        y: measure.weigth
+      }
+      dataPoints.push(dataPoint);
+    });
+
+    return dataPoints;
+  };
 
   private getSuggestedMax(): number {
     if (this.data) {
