bb4f22a46d354cf0b86dac9b76b309e5d0f85de6
ORN-Fox <49326517+ORN-Fox@users.noreply.github.com>
2025-09-12 20:47:09 -0400

    feat: added enabled properties in pageConfig in home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,6 +4,7 @@ interface IPageConfig {
   path: string;
   title: string;
   iconSrc: string;
+  enabled: boolean;
 }
 
 @Component({
@@ -18,11 +19,12 @@ export class HomeComponent {
 
   constructor() {
     this.pages = [
-      { path: 'informations', title: 'Informations', iconSrc: 'https://cdn-icons-png.flaticon.com/128/8334/8334289.png' },
-      { path: 'weigth', title: 'Suivi du poids', iconSrc: 'https://cdn-icons-png.flaticon.com/128/9184/9184519.png' },
-      { path: 'notes', title: 'Notes', iconSrc: 'https://cdn-icons-png.flaticon.com/128/2865/2865447.png' },
-      { path: 'vaccines', title: 'Vaccins', iconSrc: 'https://cdn-icons-png.flaticon.com/128/1303/1303467.png' },
-      { path: 'settings', title: 'Parametres', iconSrc: 'https://cdn-icons-png.flaticon.com/128/7216/7216457.png' }
+      { path: 'informations', title: 'Informations', iconSrc: 'https://cdn-icons-png.flaticon.com/128/8334/8334289.png', enabled: true },
+      { path: 'weigth', title: 'Suivi du poids', iconSrc: 'https://cdn-icons-png.flaticon.com/128/9184/9184519.png', enabled: true },
+      { path: 'notes', title: 'Notes', iconSrc: 'https://cdn-icons-png.flaticon.com/128/2865/2865447.png', enabled: true },
+      { path: 'vaccines', title: 'Vaccins', iconSrc: 'https://cdn-icons-png.flaticon.com/128/1303/1303467.png', enabled: true },
+      { path: 'antiflea', title: 'Vermifuges', iconSrc: 'https://cdn-icons-png.flaticon.com/128/4216/4216649.png', enabled: false },
+      { path: 'settings', title: 'Parametres', iconSrc: 'https://cdn-icons-png.flaticon.com/128/7216/7216457.png', enabled: false }
     ];
   }
 
