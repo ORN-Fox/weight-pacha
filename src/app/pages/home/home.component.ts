2c5d0a826953304b49272ee02917ac733c23314e
ORN-Fox <49326517+ORN-Fox@users.noreply.github.com>
2025-09-13 21:29:39 -0400

    feat: review home pages UX and pages title UX with dedicated image

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 interface IPageConfig {
   path: string;
   title: string;
-  iconSrc: string;
   enabled: boolean;
 }
 
@@ -19,12 +18,12 @@ export class HomeComponent {
 
   constructor() {
     this.pages = [
-      { path: 'informations', title: 'pages.informations.title', iconSrc: 'https://cdn-icons-png.flaticon.com/128/8334/8334289.png', enabled: true },
-      { path: 'weigth', title: 'pages.weightMonitoring.title', iconSrc: 'https://cdn-icons-png.flaticon.com/128/9184/9184519.png', enabled: true },
-      { path: 'notes', title: 'pages.notes.title', iconSrc: 'https://cdn-icons-png.flaticon.com/128/2865/2865447.png', enabled: true },
-      { path: 'vaccines', title: 'pages.vaccines.title', iconSrc: 'https://cdn-icons-png.flaticon.com/128/1303/1303467.png', enabled: true },
-      { path: 'antiflea', title: 'pages.antiflea.title', iconSrc: 'https://cdn-icons-png.flaticon.com/128/4216/4216649.png', enabled: false },
-      { path: 'settings', title: 'pages.settings.title', iconSrc: 'https://cdn-icons-png.flaticon.com/128/7216/7216457.png', enabled: false }
+      { path: 'informations', title: 'informations', enabled: true },
+      { path: 'weight', title: 'weight', enabled: true },
+      { path: 'notes', title: 'notes', enabled: true },
+      { path: 'vaccines', title: 'vaccines', enabled: true },
+      { path: 'antiflea', title: 'antiflea', enabled: false },
+      { path: 'settings', title: 'settings', enabled: false }
     ];
   }
 
