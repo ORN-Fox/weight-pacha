856601a75c5703f23d565cb4f5454d811993c3d8
ORN_Fox <foxdog.orion@gmail.com>
2025-09-21 18:42:29 -0400

    feat: create serializer service for centralize code and reuse

---
+++ src/app/core/services/serializer/serializer.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { isEmpty } from 'lodash';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class SerializerService {
+
+    constructor() {
+
+    }
+
+    serializeList(list: any[]) {
+        if (isEmpty(list)) {
+            return list;
+        }
+
+        let serializeList: any[] = [];
+        list.forEach(item => {
+            serializeList.push(item.serializeForSave());
+        });
+        return serializeList;
+    }
+
+}
