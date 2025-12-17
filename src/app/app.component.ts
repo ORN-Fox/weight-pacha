73af50a039d4c1ae36e793cb121bbb4b7746d8d4
ORN-Fox <49326517+ORN-Fox@users.noreply.github.com>
2025-12-17 10:31:58 -0500

    chore: clean up code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject, OnInit } from '@angular/core';
-
 import { Router } from '@angular/router';
 
 import { AuthService } from 'src/app/core/services/auth/auth.service';
--- src/app/core/services/api/api.service.ts
+++ src/app/core/services/api/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 
