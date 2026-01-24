6a6d4a11f5bd825e755c9583957fcba3b2d743cf
ORN-Fox <49326517+ORN-Fox@users.noreply.github.com>
2026-01-24 18:16:09 -0500

    feat: add archive and restore pet record action in account page

--- src/app/pages/account/tabs/account-tab-informations/account-tab-informations.component.ts
+++ src/app/pages/account/tabs/account-tab-informations/account-tab-informations.component.ts
@@ -1,7 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { catchError, Subscription, tap, throwError } from 'rxjs';
 
+import { ApiService } from 'src/app/core/services/api/api.service';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { ToastService } from 'src/app/core/services/toast/toast.service';
+import { TranslateService } from '@ngx-translate/core';
 
+import { PetRecord } from 'src/app/core/models/pet-record/pet-record.model';
 import { User } from 'src/app/core/models/user/user.model';
 
 @Component({
@@ -10,14 +15,49 @@ import { User } from 'src/app/core/models/user/user.model';
   styleUrl: './account-tab-informations.component.scss',
   standalone: false
 })
-export class AccountTabInformationsComponent {
+export class AccountTabInformationsComponent implements OnDestroy {
 
+  readonly apiService = inject(ApiService);
   readonly authService = inject(AuthService);
+  readonly toastService = inject(ToastService);
+  readonly translateService = inject(TranslateService);
+
+  archiveOrRestorePetRecordSub: Subscription;
 
   user: User;
 
   constructor() {
     this.user = this.authService.userValue;
+  }
+
+  ngOnDestroy() {
+    this.archiveOrRestorePetRecordSub?.unsubscribe();
+  }
+
+  restorePetRecord(petRecord: PetRecord) {
+    this.archiveOrRestorePetRecord(false, petRecord);
+  }
+
+  archivePetRecord(petRecord: PetRecord) {
+    this.archiveOrRestorePetRecord(true, petRecord);
+  }
+
+  private archiveOrRestorePetRecord(archive: boolean, petRecord: PetRecord) {
+    this.toastService.showConfirm().then((result: { isConfirmed: boolean; }) => {
+      if (result.isConfirmed && petRecord) {
+        const action = archive ? 'archive' : 'restore';
+        this.archiveOrRestorePetRecordSub = this.apiService.put(`/pet-record/${ petRecord.id }/${ action }`, petRecord).pipe(
+          tap(() => {
+            // TODO handle loading animation and pet record achive display
+          }),
+          catchError((error) => {
+            this.toastService.showToast('error', this.translateService.instant(`commons.toast.error.${ action }`));
+            console.error(`Unable to ${ action } pet record`, error);
+            return throwError(() => error);
+          }),
+        ).subscribe();
+      }
+    });
   }
 
 }
