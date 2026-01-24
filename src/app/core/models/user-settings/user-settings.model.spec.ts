0bb1062be7b5961815006cfbfa399b4b4e3fa5b9
ORN-Fox <49326517+ORN-Fox@users.noreply.github.com>
2026-01-24 18:06:19 -0500

    feat: rename model settings to user-settings

---
+++ src/app/core/models/user-settings/user-settings.model.spec.ts
@@ -0,0 +1,8 @@
+import { UserSettings } from "./user-settings.model";
+
+
+describe('UserSettings', () => {
+    it('should create an instance', () => {
+        expect(new UserSettings()).toBeTruthy();
+    });
+});
---
+++ src/app/core/models/user-settings/user-settings.model.ts
@@ -0,0 +1,52 @@
+import { ISerializeModel, SerializeModel } from "../serialize.model";
+
+export interface ISerializedUserSettings extends ISerializeModel {
+    locale: string;
+    theme?: string;
+    calendarViewFormat?: string;
+    itemsPerPage?: number;
+    weightUnit?: number;
+}
+
+export class UserSettings extends SerializeModel {
+
+    locale: string;
+    theme?: string;
+    calendarViewFormat?: string;
+    itemsPerPage?: number;
+    weightUnit?: number;
+
+    constructor(locale: string = 'en-US') {
+        super();
+        
+        this.locale = locale;
+    }
+
+    override serializeForSave(): ISerializedUserSettings {
+        let serializedFields = {
+            locale: this.locale,
+            theme: this.theme,
+            calendarViewFormat: this.calendarViewFormat,
+            itemsPerPage: this.itemsPerPage,
+            weightUnit: this.weightUnit
+        };
+
+        let serializedUserSettings: ISerializedUserSettings = Object.assign(serializedFields, super.serializeForSave());
+
+        return serializedUserSettings;
+    }
+
+    override deserilizeFromSave(serializedUSerSettings: ISerializedUserSettings) {
+        try {
+            super.deserilizeFromSave(serializedUSerSettings);
+            
+            this.locale = serializedUSerSettings.locale;
+            this.theme = serializedUSerSettings.theme;
+            this.calendarViewFormat = serializedUSerSettings.calendarViewFormat;
+            this.itemsPerPage = serializedUSerSettings.itemsPerPage;
+            this.weightUnit = serializedUSerSettings.weightUnit;
+        } catch (exception) {
+            console.error('Exception on deserialize user settings model', exception);
+        }
+    }
+}
